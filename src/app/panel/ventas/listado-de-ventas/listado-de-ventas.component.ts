40bf034751a0e9b28574d47b8ab9a7c64c475090
Nahuel Moyano <naku.24moyano@gmail.com>
2024-12-31 19:28:42 -0300

    listado venta listo

--- src/app/panel/ventas/listado-de-ventas/listado-de-ventas.component.ts
+++ src/app/panel/ventas/listado-de-ventas/listado-de-ventas.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Venta } from 'src/app/models/venta/venta';
+import { VentasService } from 'src/app/services/ventas/ventas.service';
 
 @Component({
   selector: 'app-listado-de-ventas',
   templateUrl: './listado-de-ventas.component.html',
-  styleUrls: ['./listado-de-ventas.component.css']
+  styleUrls: ['./listado-de-ventas.component.css'],
 })
-export class ListadoDeVentasComponent {
+export class ListadoDeVentasComponent implements OnInit {
+  @Input() ventas: Venta[] = [];
 
+  totalRecords: any;
+  loading: boolean = false;
+
+  constructor(
+    private ventasService: VentasService,
+    private toastr: ToastrService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.getAllDatos();
+  }
+
+  // d***************************************** FUNCION PARA OBTENER TODOS LOS DATOS ***********************************
+  getAllDatos() {
+    this.loading = true;
+    this.ventasService.getAllDataVentas().subscribe(
+      (data: any) => {
+        this.ventas = data.resultado;
+        console.log(this.ventas);
+        this.loading = false;
+        this.totalRecords = this.ventas?.length;
+      },
+      (error) => {
+        console.error('Error al obtener los ventas:', error);
+      }
+    );
+  }
+
+  // d***************************************** FUNCION PARA ELIMINAR ***********************************
+  eliminar(marca: any) {
+    this.ventasService.deleteData(marca).subscribe({
+      next: (response: any) => {
+        if (response.statusCode === 204) {
+          this.toastr.success('¡La venta se ha eliminado correctamente!');
+          this.getAllDatos();
+        } else {
+        }
+      },
+      error: (error: any) => {
+        this.toastr.error(
+          'Ha ocurrido un error al intentar eliminar la marca.'
+        );
+      },
+    });
+  }
+
+  // d***************************************** FUNCION PARA OBTENER EL TOTAL DE DATOS ***********************************
+  obtenerTotalResultados(): number {
+    return this.ventas?.length;
+  }
+
+  // d***************************************** FUNCION PARA REDIRECCIONAR A AGREGAR PROYECTO ***********************************
+  navigateToVenta() {
+    this.router.navigate(['/admin/ventas/cargar-venta']);
+  }
 }
--- src/app/services/ventas/ventas.service.ts
+++ src/app/services/ventas/ventas.service.ts
@@ -10,6 +10,8 @@ export class VentasService {
   private API_URL =
     'https://apicaja.junasoft.com/MovimientosCaja/IngresosDelDia';
 
+  private API_URL_VENTAS = 'https://apicaja.junasoft.com/Ventas';
+
   private API_URL_FACTURACION_MES =
     'https://apicaja.junasoft.com/MovimientosCaja/FacturacionDelMes';
 
@@ -20,8 +22,31 @@ export class VentasService {
     return this.http.get<Venta[]>(this.API_URL);
   }
 
+  // MOSTRAR DATOS VENTAS
+  getAllDataVentas(): Observable<Venta[]> {
+    return this.http.get<Venta[]>(this.API_URL_VENTAS);
+  }
+
   // MOSTRAR DATOS FACTUACION DELMES
   getAllFacturacionDelMes(): Observable<Venta[]> {
     return this.http.get<Venta[]>(this.API_URL_FACTURACION_MES);
+  }
+
+  // ACTUALIZAR VENTA
+  updateData(venta: Venta): Observable<Venta> {
+    return this.http.put<Venta>(
+      `${this.API_URL_VENTAS}/${venta.idVenta}`,
+      venta
+    );
+  }
+
+  // GET BY ID
+  getDataById(id: number): Observable<Venta> {
+    return this.http.get<Venta>(this.API_URL_VENTAS + '/' + id);
+  }
+
+  // ELIMINAR DATOS
+  deleteData(venta: Venta): Observable<Venta[]> {
+    return this.http.delete<Venta[]>(this.API_URL_VENTAS + '/' + venta.idVenta);
   }
 }
